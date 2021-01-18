47dc3f9527a77d90549f7f121f8d28e26061176e
unknown <paulclue20@gmail.com>
2021-01-18 12:03:39 -0500

    add welcome message

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,11 @@
 #!/usr/bin/env ruby
 
-puts 'Hello World'
+puts "Welcome to Tic-Tac-Toe.\nPlayer one please enter Your name."
+player_1 = gets.chomp
+puts "Now player two please enter your name."
+player_2 = gets.chomp
+puts "\n"
+puts "Hello #{player_1} and #{player_2}!\n"
+puts "In order to win the game,\na player must place three of their marks in a\nhorizontal, vertical, or diagonal row.\n"
+puts "#{player_1} please choose your mark by entering \"x\" or \"o\" below.\n"
+player_1_mark = gets.chomp
