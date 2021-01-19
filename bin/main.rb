77c9ccea3bfd7b86a0bb1ffc37f23036b2dc83b0
Ismael Antonio <ismael@pop-os.localdomain>
2021-01-19 10:13:07 +0300

    add winning msg

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
-lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+require_relative '../lib/interface'
+
 # winning_combo = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
 puts "Welcome to Tic-Tac-Toe.\nPlayer one please enter Your name."
@@ -15,29 +17,72 @@ puts "\n"
 puts "#{player_one} please choose your mark by entering \"x\" or \"o\" below.\n"
 player_one_mark = gets.chomp
 
-player_two_mark = if player_one_mark == 'x'
-                    'o'
-                  else
-                    'x'
-                  end
+game_on = true
 
-puts "\n"
-puts "#{player_one} your mark is -- #{player_one_mark}."
-puts "And #{player_two} your mark is -- #{player_two_mark}."
-puts "\n"
-puts "\n"
-lines.each do |line|
-  puts "#{line} "
+while game_on
+  player_two_mark = if player_one_mark == 'x'
+                      'o'
+                    else
+                      'x'
+                    end
+
+  puts "\n"
+  puts "#{player_one} your mark is -- #{player_one_mark}."
+  puts "And #{player_two} your mark is -- #{player_two_mark}."
+  puts "\n"
+  puts "\n"
+  board = Players.new
+  board.updated_board.each do |line|
+    line.each do |char|
+      print "| #{char} |"
+    end
+    puts
+  end
+  repeat_loop = true
+  while repeat_loop
+    puts "\n"
+    puts "#{player_one} your turn. Enter the number of where you would like to place your mark. "
+    played_num = gets.chomp.to_i
+    board.player_move(played_num, player_one_mark)
+
+    player_one_num = Players.new
+    player_one_num.player_move(played_num, player_one_mark)
+
+    check = player_one_num.valid(played_num)
+    if check == true
+      puts 'number taken, please enter a valid number'
+      repeat_loop = true
+    else
+      repeat_loop = false
+      puts "#{player_two} your turn. Enter the number of where you would like to place your mark. "
+      played_num = gets.chomp.to_i
+      board.player_move(played_num, player_two_mark)
+
+      player_two_num = Players.new
+      player_two_num.player_move(played_num, player_two_mark)
+
+      check = player_two_num.valid(played_num)
+      if check == true
+        puts 'number taken, please enter a valid number'
+        repeat_loop = true
+      else
+        repeat_loop = false
+        game_on = false
+      end
+    end
+  end
+  #   game_on = false if winner # and/or draw (the exact condition in this milestone is not important)
+  if is_winner == player_one
+    #     puts "\n"
+    puts "*********#{player_one}wins the match!******"
+  elsif puts "\n"
+    puts "********#{player_two} wins the match!******"
+  else
+    #     puts "\n"
+    puts 'It is a draw ! do you wan play again?'
+    puts 'y / n'
+    response = gets.chomp
+    unless response game_on = false
+    end
+  end
 end
-puts "\n"
-puts "#{player_one} your turn. Enter the number of where you would like to place your mark. "
-# mark = gets.chomp
-
-puts "#{player_two} your turn. Enter the number of where you would like to place your mark. "
-# mark = gets.chomp
-
-puts "\n"
-
-puts "#{player_one} wins the match!"
-
-puts "#{player_two} wins the match!"
